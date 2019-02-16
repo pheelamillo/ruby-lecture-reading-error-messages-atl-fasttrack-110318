c1d3745b50e0a81ad4d913d0a688469b8fe9dc31
Pheelamillo Ghajiga <pheelamillo@gmail.com>
2019-02-16 18:43:44 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,10 @@
 puts "Welcome to fastrack"
+
+def say_message(greeted)
+  message = "Hello" + greeted.to_s + "great to meet you!"
+  puts message 
+end
+
+10.times do |step|
+  say_message (step)
+end
